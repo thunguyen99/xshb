49876078446e9da36fc735b9f8872827f265c337
lijian <lijian@lijianmatoMacBook-Pro.local>
2011-01-01 02:22:16 +0800

    add jrails

--- app/controllers/order_line_items_controller.rb
+++ app/controllers/order_line_items_controller.rb
@@ -1,2 +1,9 @@
 class OrderLineItemsController < ApplicationController
+  before_filter :login_required
+
+  def create
+    puts "@@@@@@@@@"
+    render :text => "ok"
+  end
+
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,7 @@ class ProductsController < ApplicationController
   before_filter :login_required
 
   def index
+    @order_line_item = OrderLineItem.new
   end
 
 end
