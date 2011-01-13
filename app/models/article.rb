831bf6682bb0b6ead27e9e7da3e010e5a0fac2c4
lijian <lijian@lijianmatoMacBook-Pro.local>
2011-01-13 14:06:50 +0800

    list contents

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,8 @@ class Article < ActiveRecord::Base
 
   validates_presence_of :title,:category_id
 
+  named_scope :published, :conditions => ["is_published=1"]
+
   def show_status
     if self.is_published
       "<img src='/images/published.gif' />"
--- app/models/panel_content.rb
+++ app/models/panel_content.rb
@@ -3,6 +3,12 @@ class PanelContent < ActiveRecord::Base
 
   validates_presence_of :title,:panel_category_id
 
+  named_scope :published, :conditions => ["is_published=1"]
+
+  named_scope :in_category_name, lambda { |category_name|
+    { :include=> [:panel_category],:conditions => ["panel_categories.name = ?", category_name] }
+  }
+
   has_attached_file :uploaded_data,
                     :default_url => "",
                     :url => "/images/assets/:attachment/:id/:style/:filename",
@@ -22,4 +28,9 @@ class PanelContent < ActiveRecord::Base
       "<img src='/images/draft.gif' />"
     end
   end
+
+  def self.list_contents(category,limit=nil)
+    self.in_category_name(category).published.all(:order=>"weight DESC")
+  end
+
 end
